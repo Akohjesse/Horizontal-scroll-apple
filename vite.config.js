e7a4f12b85e43c82f17a69bd04f9ad16da0fb880
⚡️ <akohgodwinjesse@gmail.com>
2023-10-04 19:23:55 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host:true
+  },
+  resolve: {
+    alias: {
+       '@': path.resolve(__dirname, 'src')
+     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "./src/styles/abstracts/_mixins.scss";
+        @import "./src/styles/abstracts/_variables.scss";
+        `
+      }
+    }
+  }
+})
